b740bf226f66e49077c00efafefd8d93678dfd28
Phineas Kibbey <1381816+pkibbey@users.noreply.github.com>
2025-05-31 04:45:47 -0700

    update windows workers

--- src/workers/start-windows-workers.ts
+++ src/workers/start-windows-workers.ts
@@ -4,12 +4,7 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: '.env.local' });
 
 // Import all worker modules
-import './object-detection-worker';
-import './advanced-analysis-worker';
-import './content-warnings-worker';
 import './duplicates-worker';
-import './exif-worker';
-import './thumbnail-worker';
 
 console.log(
 	'All workers have been started and are listening to their respective queues.',
